6c908c640cafb5877545475b4715ec3f292c18f3
elninowang <elninowang@qq.com>
2017-10-07 11:20:13 +0800

    Complete the project

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+    public static int CoinCount = 0;
+
+    void Awake()
+    {
+        ++Coin.CoinCount;
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("Entered Collider");
+        if (col.CompareTag("Player"))
+            Destroy(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        --Coin.CoinCount;
+        if (Coin.CoinCount <= 0)
+        {
+            // Win
+            GameObject Timer = GameObject.Find("LevelTimer");
+            Destroy(Timer);
+            GameObject[] FireworkSystems = GameObject.FindGameObjectsWithTag("Fireworks");
+            foreach (GameObject GO in FireworkSystems)
+                GO.GetComponent<ParticleSystem>().Play();
+        }
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+    public float MaxTime = 60f;
+
+    [SerializeField]
+    public float CountDown = 0;
+
+	// Use this for initialization
+	void Start () {
+        CountDown = MaxTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CountDown -= Time.deltaTime;
+
+        if (CountDown < 0)
+        { 
+            Coin.CoinCount = 0;
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
